db1619dcd539a7fcfab0f6b1cc57c3942cff0af0
mac <franquicidad@gmail.com>
2018-05-23 21:16:21 -0500

    adding layouts

---
+++ app/src/main/java/com/example/mac/francosbakingapp/IngredientActivity.java
@@ -0,0 +1,15 @@
+package com.example.mac.francosbakingapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class IngredientActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+}
--- app/src/main/java/com/example/mac/francosbakingapp/MainActivity.java
+++ app/src/main/java/com/example/mac/francosbakingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mac.francosbakingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -58,6 +59,6 @@ public class MainActivity extends AppCompatActivity implements MainActAdapter.Re
 
     @Override
     public void onRecipeClick(Recipe recipe) {
-
+        Intent IngredientsIntent =new Intent(this,)
     }
 }
