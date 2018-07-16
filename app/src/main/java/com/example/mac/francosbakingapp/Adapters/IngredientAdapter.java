08480f29173998775ba1f385c384521d8a674180
mac <franquicidad@gmail.com>
2018-07-16 18:56:09 -0500

    implements the tablet

--- app/src/main/java/com/example/mac/francosbakingapp/Adapters/IngredientAdapter.java
+++ app/src/main/java/com/example/mac/francosbakingapp/Adapters/IngredientAdapter.java
@@ -42,6 +42,7 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.in
         mIngredientList = ingredientList;
         notifyDataSetChanged();
     }
+    //guachu
 
     public class ingredientAdapterViewHolder extends RecyclerView.ViewHolder {
 
